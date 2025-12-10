571cfcd53e625e3cf3e6b2cbc8508db3d8b6531f
Nisrina Nur Fatin <nisrinanurfatin@gmail.com>
2025-12-10 13:30:44 +0700

    db

--- src/config/db.js
+++ src/config/db.js
@@ -5,11 +5,10 @@ dotenv.config()
 const { Pool } = pkg
 
 const pool = new Pool ({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT
+    connectionString: process.env.POSTGRES_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 export default pool
