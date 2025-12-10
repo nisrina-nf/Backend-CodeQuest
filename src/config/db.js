8c21df74622f06bcd9ad0fc335fb819743774b20
Nisrina Nur Fatin <nisrinanurfatin@gmail.com>
2025-12-10 14:51:57 +0700

    db2

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,5 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
 import pkg from 'pg'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -5,7 +7,11 @@ dotenv.config()
 const { Pool } = pkg
 
 const pool = new Pool ({
-    connectionString: process.env.POSTGRES_URL
+    connectionString: process.env.POSTGRES_URL_NON_POOLING,
+    ssl: {
+        rejectUnauthorized: false,
+        require: true
+    }
 })
 
 export default pool
